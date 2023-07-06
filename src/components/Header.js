bb1aa91b531b3e9e7e3a49ce283fcada2db3b0af
Kris1027 <obarzanek.work@gmail.com>
2023-07-06 10:18:36 +0200

    navigation toggle function

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Nav from './Nav';
 
 function Header() {
+
+    const [isActive, setIsActive] = useState(false);
+
+    const toggleClass = () => {
+        setIsActive(!isActive);
+    }
+
     return (
         <header className='header'>
             <div className='logo'>
                 <div className='logo__image'></div>
             </div>
-            <div className="hamburger">
-                <div className="hamburger__image"></div>
+            <div className='hamburger'>
+                <div className={isActive? 'hamburger__image active' : 'hamburger__image'} onClick={toggleClass}></div>
             </div>
+            <Nav isActive={isActive} toggleClass={toggleClass}/>
         </header>
     )
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-function Nav() {
+function Nav({ isActive, toggleClass}) {
+
+    const menuToggle = isActive ? 'menu active' : 'menu';
+
     const navCategories = ['how we work', 'blog', 'account', 'view plans'];
     return (
-        <nav className='menu'>
+        <nav className={menuToggle} onClick={toggleClass}>
             {navCategories.map((category, index) => (
                 <button key={index} className='menu__btn'>{category}</button>
             ))};
