128e5fb7b876fca91c9c0da42804a9d60fd06712
Kris1027 <obarzanek.work@gmail.com>
2023-07-15 19:16:51 +0200

    fix wrong concept of adding images

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,24 +2,28 @@ import React, { useState } from 'react';
 import Nav from './Nav';
 
 function Header() {
+  const [isActive, setIsActive] = useState(false);
 
-    const [isActive, setIsActive] = useState(false);
+  const toggleClass = () => {
+    setIsActive(!isActive);
+  };
 
-    const toggleClass = () => {
-        setIsActive(!isActive);
-    }
-
-    return (
-        <header className='header'>
-            <div className='logo'>
-                <div className='logo__image'></div>
-            </div>
-            <div className='hamburger'>
-                <div className={isActive? 'hamburger__image active' : 'hamburger__image'} onClick={toggleClass}></div>
-            </div>
-            <Nav isActive={isActive} toggleClass={toggleClass}/>
-        </header>
-    )
+  return (
+    <header className='header'>
+      <div className='logo'>
+        <div className='logo__image'>
+          <img src='./images/logo.svg' alt='logo' />
+        </div>
+      </div>
+      <div className='hamburger'>
+        <div
+          className={isActive ? 'hamburger__image active' : 'hamburger__image'}
+          onClick={toggleClass}
+        ></div>
+      </div>
+      <Nav isActive={isActive} toggleClass={toggleClass} />
+    </header>
+  );
 }
 
 export default Header;
