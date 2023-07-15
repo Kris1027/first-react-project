c0ce056521e17faaed470f55d844ff1d8b54ff1c
Kris1027 <obarzanek.work@gmail.com>
2023-07-15 19:35:44 +0200

    nav img fix

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,24 @@
 import React from 'react';
 
 function Nav({ isActive }) {
+  const menuToggle = isActive ? 'menu active' : 'menu';
 
-    const menuToggle = isActive ? 'menu active' : 'menu';
-
-    const navCategories = ['how we work', 'blog', 'account', 'view plans'];
-    return (
-        <nav className={menuToggle}>
-            {navCategories.map((category, index) => (
-                <button key={index} className='menu__btn'>{category}</button>
-            ))};
-            <div className='menu__image'></div>
-        </nav>
-    )
+  const navCategories = ['how we work', 'blog', 'account', 'view plans'];
+  return (
+    <nav className={menuToggle}>
+      {navCategories.map((category, index) => (
+        <button key={index} className='menu__btn'>
+          {category}
+        </button>
+      ))}
+      ;
+      <img
+        className='menu__image'
+        src='./images/bg-pattern-mobile-nav.svg'
+        alt='menu background'
+      />
+    </nav>
+  );
 }
 
 export default Nav;
